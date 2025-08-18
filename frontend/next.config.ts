fc8040f9260ce5a0b5b439c012726fe2e172c440
Jacob-Walton <jacob-walton@konpeki.co.uk>
2025-08-18 02:08:56 +0100

    fix: add remotePatterns

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,6 +3,22 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || ''
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.jsdelivr.net',
+        port: '',
+        pathname: '/gh/devicons/devicon@latest/icons/**'
+      },
+      {
+        protocol: 'https',
+        hostname: 'd1tl6qv7xwsvxx.cloudfront.net',
+        port: '',
+        pathname: '/assets/**'
+      }
+    ]
   }
 };
 
